4935762e1b3e461b25b15c904f3035494bb8258b
Abraham Arce <xe1gyq@gmail.com>
2014-03-01 13:32:37 -0600

    Modules: Weather, Sunset + Fixes
    Add Sunset information + minor fixes and cleanup

--- modules/weather.py
+++ modules/weather.py
@@ -8,16 +8,11 @@ from voicesynthetizer import VoiceSynthetizer
 
 class Weather:
 
-	def __init__(self, method):
-		self.method = method
+	def __init__(self):
 		self.url = "http://weather.yahooapis.com/forecastrss?w=124162&u=c"
 
 		self.speaker = VoiceSynthetizer("festival", "spanish")
 		self.parser(self.url)
-
-	def setup(self, method):
-		self.method = method
-		return
 
 	def parser(self, url):
 
@@ -34,9 +29,7 @@ class Weather:
 		sunrise=self.astronomy['sunrise']
 		sunset=self.astronomy['sunset']
 
-		print data
-		summary=d.entries[0].summary
-		print summary
+		summary=data.entries[0].summary
 		temp=summary.split("High: ")
 		temp=temp[1].split(" Low: ")
 		self.high=temp[0]
@@ -48,7 +41,7 @@ class Weather:
 		self.speaker.speechit("Temperatura maxima, " + self.high + " grados centigrados")
 		self.speaker.speechit("Temperatura minima, " + self.low + " grados centigrados")
 		self.speaker.speechit("Humedad, " + self.atmosphere['humidity'] + " por ciento")
-		self.speaker.speechit("El Sol se ocultara a las " + self.astronomy['sunset'])
+		self.speaker.speechit("El Sol se ocultara a las " + self.astronomy['sunset'].replace(":", " ") + " de la tarde")
 		return
 
 if __name__ == '__main__':
