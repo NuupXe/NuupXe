356ee357853fdf8c0ea756cfebfcb5865f4b4072
Abraham Arce <xe1gyq@gmail.com>
2014-03-01 11:45:46 -0600

    Learning: Morse Teacher golearn gocompete
    2 functions defined
     - golearn, to teach morse code by sending random numbers and letters
     - gocompete, to send a text so ham radio operator can decode and compete

--- learning/morseteacher.py
+++ learning/morseteacher.py
@@ -20,15 +20,15 @@ class MorseTeacher:
 		self.speaker = VoiceSynthetizer("festival", "spanish")
 		self.phonetic = Phonetic()
 
-		self.message("Modulo de Aprendizaje de Codigo Morse")
-		self.message("Sugerencias escribir a")
-		self.message("".join(self.phonetic.decode('xe1gyq@gmail.com')))
-
 	def __del__(self):
 		pass
 
 	def message(self, message):
 		self.speaker.speechit(message)
+
+	def welcome(self):
+		self.message("Modulo de Aprendizaje de Codigo Morse, sugerencias escribir a ")
+		self.message("".join(self.phonetic.decode('xe1gyq@gmail.com')))
 
 	def randomnumber(self, n):
 		range_start = 10**(n-1)
@@ -43,22 +43,48 @@ class MorseTeacher:
 
 	def randomnumberplay(self, help, iterations):
 		random = self.randomnumber(iterations)
+		self.message("Seccion de Prueba de Numeros Aleatorios")
 		for x in str(random):
+			if not help:
+				self.morse.generate(x)
+				self.message("El numero codificado fue, " + x + ", " + "".join(self.phonetic.decode(x)))
 			if help:
-				self.speaker.speechit("Numero " + x + ", " + "".join(self.phonetic.decode(x)))
-				self.speaker.speechit("Su codificacion es ")
-			self.morse.generate(x)
+				self.message("El numero, " + x + ", " + "".join(self.phonetic.decode(x)) + "codificado es, ")
+				self.morse.generate(x)
+			time.sleep(1)
 
 	def randomletterplay(self, help, iterations):
 		random = self.randomletter(iterations)
+		self.message("Seccion de Prueba de Letras Aleatorias")
 		for x in str(random):
+			if not help:
+				self.morse.generate(x)
+				self.message("La letra codificada fue, " + x + ", " + "".join(self.phonetic.decode(x)))
 			if help:
-				self.speaker.speechit("Letra " + x + ", " + "".join(self.phonetic.decode(x)))
-				self.speaker.speechit("Su codificacion es ")
-			self.morse.generate(x)
+				self.message("La letra, " + x + ", de " + "".join(self.phonetic.decode(x)) + "codificada es, ")
+				self.morse.generate(x)
+			time.sleep(1)
+
+	def golearn(self):
+		print '[Cancun] Morse Teacher Learning'
+		self.welcome()
+		self.randomnumberplay(True, 10)
+		self.randomnumberplay(False, 10)
+		self.message("Hagamos una pausa de 10 segundos")
+		time.sleep(10)
+		self.randomletterplay(True, 10)
+		self.randomletterplay(False, 10)
+
+	def gocompete(self):
+		print '[Cancun] Morse Teacher Contest'
+		self.message("Concurso Decodificando Texto")
+		self.message("Envia tus resultados a " + "".join(self.phonetic.decode('xe1gyq@gmail.com')))
+		self.message("El primer correo con el texto correcto sera ganador de una tarjeta de 200 pesos para usar en Star Bocs")
+		self.message("Listo? Comenzamos...")
+		return
 
 if __name__ == '__main__':
 
 	mymorse = MorseTeacher()
-	mymorse.randomnumberplay(True, 5)
-	mymorse.randomletterplay(True, 5)
+	mymorse.golearn()
+	mymorse.gocompete()
