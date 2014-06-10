0d31d3d8067c06948fef2582378e491cd0184c3e
Abraham Arce <xe1gyq@gmail.com>
2014-06-09 20:46:25 -0500

    Modules: Wolfram Initialization
    Initialization and configuration path changes

--- modules/wolfram.py
+++ modules/wolfram.py
@@ -12,21 +12,23 @@ class Wolfram(object):
     def __init__(self, voicesynthetizer):
 
         self.phonetic = Phonetic()
-        self.voicesynthetizer = VoiceSynthetizer("google", "english")
+        self.voicesynthetizer = voicesynthetizer
         
         self.conf = ConfigParser.ConfigParser()
-        self.path = "../configuration/wolfram.config"
+        self.path = "configuration/wolfram.config"
         self.conf.read(self.path)
 
     def identify(self):
         print '[Cancun] Wolfram'
         appid=self.conf.get("wolfram", "appid")
-	client = wolframalpha.Client(appid)
-	res = client.query('how many words can someone say in one day')
-	print(next(res.results).text)
+        client = wolframalpha.Client(appid)
+        question = 'how many grams in kilograms'
+        res = client.query(question)
+        self.voicesynthetizer.speechit(question)
+        print(next(res.results).text)
         self.voicesynthetizer.speechit(next(res.results).text)
 
 if __name__ == '__main__':
 
-    mytest = Wolfram("google")
+    mytest = Wolfram()
     mytest.identify()
