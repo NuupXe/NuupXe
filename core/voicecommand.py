4b6eddd875d6356ed2960622fb2d1cc1a945acb0
Abraham Arce <xe1gyq@gmail.com>
2014-09-16 20:28:28 -0500

    Core: VoiceCommand Audio Files
    Rename audio files used

--- core/voicecommand.py
+++ core/voicecommand.py
@@ -11,10 +11,10 @@ class VoiceCommand(object):
 
 	self.output = ""
         self.agent = "google"
-        self.audiofilewav = "audio.wav"
-        self.audiofilewavcompand = "audioc.wav"
-        self.audiofilewavnoise = "audion.wav"
-        self.audiofileflac = "audio.flac"
+        self.audiofilewav = "voicecommand.wav"
+        self.audiofilewavcompand = "voicecommandc.wav"
+        self.audiofilewavnoise = "voicecommandn.wav"
+        self.audiofileflac = "voicecommand.flac"
         self.voicesynthetizer = voicesynthetizer
         self.pushtotalk = PushToTalk()
         self.voicetospeech = VoiceToSpeech()
@@ -32,9 +32,9 @@ class VoiceCommand(object):
                 status, output = commands.getstatusoutput("arecord -vv -f cd -d " + seconds + " " + self.audiofile)
         elif self.agent == 'google':
                 status, output = commands.getstatusoutput("arecord -d " + seconds + " -f dat -t wav -r 48000 -c 2 " + self.audiofilewav)
-                status, output = commands.getstatusoutput("sox " + self.audiofilewav + " " + self.audiofilewavcompand + " compand 0.02,0.20 5:-60,-40,-10 -5 -90 0.1")
-                status, output = commands.getstatusoutput("sox " + self.audiofilewav + " -n remix 1 trim 0 1 noiseprof noise.prof")
-                status, output = commands.getstatusoutput("sox " + self.audiofilewav + " " + self.audiofilewavnoise + " remix 1 noisered noise.prof")
+                #status, output = commands.getstatusoutput("sox " + self.audiofilewav + " " + self.audiofilewavcompand + " compand 0.02,0.20 5:-60,-40,-10 -5 -90 0.1")
+                #status, output = commands.getstatusoutput("sox " + self.audiofilewav + " -n remix 1 trim 0 1 noiseprof noise.prof")
+                #status, output = commands.getstatusoutput("sox " + self.audiofilewav + " " + self.audiofilewavnoise + " remix 1 noisered noise.prof")
                 status, output = commands.getstatusoutput("flac -f -o " + self.audiofileflac + " --channels=1 --sample-rate=48000 " + self.audiofilewav)
 
     def decode(self, speech):
