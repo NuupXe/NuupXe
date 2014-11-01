669bac592fa1ea72cd3c464169bebaddee9b0435
Abraham Arce <xe1gyq@gmail.com>
2014-11-01 14:22:22 -0600

    General: Cancun Start Stop Service
    Allow Cancun Scheduler mode to be started or stopped using dtmf

---
+++ cancun.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+start() {
+    python cancun.py -s &
+    echo "Cancun Project | Started in Scheduler mode..."
+}
+ 
+stop() {
+    pid=`ps -ef | grep 'cancun.py' | awk '{ print $2 }'`
+    echo $pid
+    kill $pid
+    sleep 2
+    echo "Cancun Project | Scheduler mode killed!"
+}
+ 
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop   
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  *)
+    echo "Usage: cancun.sh {start|stop|restart}"
+    exit 1
+esac
+exit 0
