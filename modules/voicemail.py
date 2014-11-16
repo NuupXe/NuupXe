de238d8cf60dd9caba2ffdec82553f9a02f09cdc
Abraham Arce <xe1gyq@gmail.com>
2014-11-16 14:07:16 -0600

    Modules: VoiceMail Not Limited to Time
    We are not now limited to a specific recording time, now we can record
    as slong as we have the ptt pressed and irlp board is recognizing it

--- modules/voicemail.py
+++ modules/voicemail.py
@@ -1,41 +1,60 @@
 #!/usr/bin/python
 
 import commands
+import time
 
+from core.irlp import Irlp
 from core.pushtotalk import PushToTalk
+from core.voice import Voice
 
 class VoiceMail(object):
 
     def __init__(self, voicesynthetizer):
+
+        self.irlp = Irlp()
+        self.pushtotalk = PushToTalk()
+        self.voice = Voice()
+
 	self.status = False
-	self.output = ""
         self.audiofilewav = "voicemail.wav"
         self.voicesynthetizer = voicesynthetizer
-        self.pushtotalk = PushToTalk()
 
     def __del__(self):
-        status, output = commands.getstatusoutput("rm " + self.audiofilewav)
+        pass
+        #status, output = commands.getstatusoutput("rm " + self.audiofilewav)
 
     def status(self):
 	return self.status
 
-    def record(self, seconds):
+    def record(self):
         print '[Cancun] Voice Mail Record'
-	self.voicesynthetizer.speechit("Graba tu mensaje en los proximos " + seconds + " segundos")
-        status, output = commands.getstatusoutput("arecord -d " + seconds + " -f dat -t wav -r 48000 -c 2 " + self.audiofilewav)
-	self.status = True
-
-    def erase(self):
-        print '[Cancun] Voice Mail Erase'
-	self.voicesynthetizer.speechit("Borrando mensaje")
-        status, output = commands.getstatusoutput("rm " + self.audiofilewav)
-	self.status = False
+	self.voicesynthetizer.speechit("Graba el mensaje")
+        time.sleep(1)
+        while self.irlp.cosenabled() is 256:
+            pass
+        while self.irlp.cosenabled() is 0:
+            pass
+        proc = self.voice.recordstart()
+        while self.irlp.cosenabled() is 256:
+            pass
+        self.voice.recordstop(proc)
 
     def play(self):
         print '[Cancun] Voice Mail Play'
         self.pushtotalk.openport()
-        status, output = commands.getstatusoutput("aplay " + self.audiofilewav)
+        self.voice.play()
         self.pushtotalk.closeport()
+
+    def erase(self):
+        print '[Cancun] Voice Mail Erase'
+	self.voicesynthetizer.speechit("Borrando el mensaje")
+        self.voice.erase()
+
+    def run(self):
+        print '[Cancun] Voice Mail Run'
+        self.record()
+        self.play()
+        self.erase()
 
 if __name__ == '__main__':
 
