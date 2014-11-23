09a6cbb063cf648e81f932392187082fc042a76e
Abraham Arce <xe1gyq@gmail.com>
2014-11-23 12:04:34 -0600

    Core: Aprs Net Base Code
    We can report data to aprs.net

---
+++ core/aprsnet.py
@@ -0,0 +1,23 @@
+import sys, time
+from socket import *
+
+class AprsNet(object):
+
+    def __init__(self):
+
+        self.serverHost = '205.233.35.52'
+        self.serverPort = 14580
+        self.password = ''
+        self.address = 'XE1GYQ-11>APRS,TCPIP*:'
+        self.position = '=2036.92N/10324.72W-'
+
+        self.packet = ''
+
+    def send_packet(self, message):
+        sSock = socket(AF_INET, SOCK_STREAM)
+        sSock.connect((self.serverHost, self.serverPort))
+        sSock.send('user XE1GYQ-11 pass ' + self.password + ' vers "XE1GYQ Cancun Project" \n')
+        sSock.send(self.address + self.position + message +'\n')
+        print("packet sent: " + time.ctime() )
+        sSock.shutdown(0)
+        sSock.close()
