77f0fd7ecc1f72fad2443fc10df83427ab9d208e
Abraham Arce <xe1gyq@gmail.com>
2014-12-04 20:27:40 -0600

    Modules: Command Top Tasks Info
    Let's report the number of tasks our irlp hots is running
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- modules/commandtop.py
+++ modules/commandtop.py
@@ -39,9 +39,22 @@ class CommandTop(object):
         data = ' '.join(self.phonetic.decode('aprs.fi/')) + ' '.join(self.phonetic.decode(callsign))
         self.voicesynthetizer.speechit("Datos de Uso de nuestra Memoria en la direccion " + data)
 
+    def tasks(self):
+        print '[Cancun] Command Top Tasks Info'
+        callsign = 'XE1GYQ-3'
+
+        self.aprs.address_set(callsign)
+        self.aprs.position_set('=2036.96N/10324.58W-')
+
+        status, output = commands.getstatusoutput("top -b -n 1 | grep Tasks")
+        self.aprs.send_packet(output)
+        data = ' '.join(self.phonetic.decode('aprs.fi/')) + ' '.join(self.phonetic.decode(callsign))
+        self.voicesynthetizer.speechit("Datos de Uso de Tareas en la direccion " + data)
+
     def execute(self):
         self.cpu()
         self.mem()
+        self.tasks()
 
 if __name__ == '__main__':
 
