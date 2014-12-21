d61fd34a733d07d220b2a44bc3c49748dffef7df
Abraham Arce <xe1gyq@gmail.com>
2014-12-21 17:01:28 -0600

    Modules: Command Clean Up
    Some clean up is required, we will use this code as a base
    for the next phase in command voice recognition

--- modules/command.py
+++ modules/command.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
 import commands
-import json
 import re
 
+from core.irlp import Irlp
 from core.voicecommand import VoiceCommand
 from core.voicesynthetizer import VoiceSynthetizer
 from core.pushtotalk import PushToTalk
@@ -31,59 +31,49 @@ class Command(object):
     def presentation(self):
         print '[Cancun] Command Presentation'
         self.voicesynthetizer.speechit("Modulo experimental de comandos por voz")
-        self.voicesynthetizer.speechit("Funciones disponibles: identificacion, hora, fecha, clima, estaciones y sismologia")
-        self.voicesynthetizer.speechit("Dinos la funcion en los proximos 5 segundos")
+        self.voicesynthetizer.speechit("Funciones disponibles: identificacion, hora, fecha, clima y sismologia")
+        self.voicesynthetizer.speechit("")
 
     def parse(self, output):
 
         if re.search(r'hola', output, re.M|re.I) or re.search(r'cancun', output, re.M|re.I):
-            print 'saludos'
             self.voicesynthetizer.speechit("Hola!, no se quien eres pero mucho gusto")
 
         if re.search(r'identif', output, re.M|re.I):
             print '[Cancun] Command Identification'
-            self.voicesynthetizer.speechit("Nos has pedido identificarnos")
             self.identification.identify()
         elif re.search(r'hora', output, re.M|re.I):
             print '[Cancun] Command Hour'
-            self.voicesynthetizer.speechit("Nos has pedido la hora")
             self.clock.hour()
         elif re.search(r'fecha', output, re.M|re.I):
             print '[Cancun] Command Date'
-            self.voicesynthetizer.speechit("Nos has pedido la fecha")
             self.clock.date()
         elif re.search(r'clima', output, re.M|re.I):
             print '[Cancun] Command Weather'
-            self.voicesynthetizer.speechit("Nos has pedido el reporte del clima")
             self.weather.report()
         elif re.search(r'sismo', output, re.M|re.I):
             print '[Cancun] Command Seismic'
-            self.voicesynthetizer.speechit("Nos has pedido el reporte sismologico")
             self.seismology.SismologicoMX()
-        elif re.search(r'estaciones', output, re.M|re.I):
-            print '[Cancun] Command Stations'
-            self.voicesynthetizer.speechit("Nos has pedido las estaciones en el area")
-            self.messages.stations()
         else:
             print '[Cancun] Command? Unknown!'
-            self.voicesynthetizer.speechit("Como que no entendimos tu comando!")
+            self.voicesynthetizer.speechit("No entendimos, hasta pronto!")
 
     def execute(self):
         self.presentation()
-        self.voicecommand.record('5')
-        output = self.voicecommand.decode('True')
+        self.voicecommand.record()
+        output = self.voicecommand.decode('False')
+        print output
         self.parse(output)
 
     def background(self):
         print '[Cancun] Background Started'
 	while True:
-	        self.voicecommand.record('5')
+	        self.voicecommand.record()
         	output = self.voicecommand.decode('False')
-		self.parse(output)
-        #if re.search(r'hola', output, re.M|re.I):
-        #     self.voicesynthetizer.speechit("Hola!, soy el proyecto Cancun")
-        #else:
-        #     print 'Negativo'
+                if re.search(r'cancun', output, re.M|re.I):
+                    self.voicesynthetizer.speechit("Alguien me hablo?. Soy el proyecto Cancun... Hasta pronto!")
+                    break
+                print output
         print '[Cancun] Background Stopped'
 
 if __name__ == '__main__':
