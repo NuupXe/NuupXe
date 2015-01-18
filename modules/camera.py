c73d4a344ddaf6df9292fd41cca6d8f9b20ea1d1
Abraham Arce <xe1gyq@gmail.com>
2015-01-18 10:30:56 -0600

    Modules: Camera Pygame Stop +
    Stops, uninitializes, and closes the camera plus some cleanup
    changes

--- modules/camera.py
+++ modules/camera.py
@@ -4,18 +4,11 @@ import commands
 import pygame
 import pygame.camera
 
-from core.irlp import Irlp
-from core.pushtotalk import PushToTalk
-from core.voice import Voice
-
 class Camera(object):
 
     def __init__(self, voicesynthetizer):
 
         self.mycamera = None
-        self.irlp = Irlp()
-        self.pushtotalk = PushToTalk()
-        self.voice = Voice()
 
         self.voicesynthetizer = voicesynthetizer
         self.picturepygame = 'output/camerapygame.jpg'
@@ -26,7 +19,7 @@ class Camera(object):
     def __del__(self):
         pass
 
-    def setup():
+    def setup(self):
         print '[Cancun] Camera Setup'
         pygame.camera.init()
         self.mycamera = pygame.camera.Camera("/dev/video0",(640,480))
@@ -38,12 +31,16 @@ class Camera(object):
         print '[Cancun] Camera Capture'
         image = self.mycamera.get_image()
         pygame.image.save(image, self.picturepygame)
-        commands.getstatusoutput(self.fswebcamcommand)
+        self.mycamera.stop()
+        status, output = commands.getstatusoutput(self.fswebcamcommand)
 
     def post(self):
         print '[Cancun] Camera Post'
-        self.pushtotalk.openport()
         self.voicesynthetizer.speechit("Foto Tomada")
-        self.pushtotalk.closeport()
+
+    def execute(self):
+        self.setup()
+        self.capture()
+        self.post()
 
 # End of file
