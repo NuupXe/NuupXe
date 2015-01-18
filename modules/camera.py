27ea2c0be7a1494f4376a2114bb2f8f56cb9f478
Abraham Arce <xe1gyq@gmail.com>
2015-01-18 10:17:34 -0600

    Modules: Camera Fswebcam
    Let's have fswebcam as our small and simple webcam command line
    application

--- modules/camera.py
+++ modules/camera.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import commands
 import pygame
 import pygame.camera
 
@@ -17,6 +18,10 @@ class Camera(object):
         self.voice = Voice()
 
         self.voicesynthetizer = voicesynthetizer
+        self.picturepygame = 'output/camerapygame.jpg'
+        self.picturefswebcam = 'output/camerafswebcam.jpg'
+        self.fswebcam = 'fswebcam'
+        self.fswebcamarguments = ' -r 1280x1024 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner '
 
     def __del__(self):
         pass
@@ -27,10 +32,13 @@ class Camera(object):
         self.mycamera = pygame.camera.Camera("/dev/video0",(640,480))
         self.mycamera.start()
 
+        self.fswebcamcommand = self.fswebcam + self.fswebcamarguments + self.picturefswebcam
+
     def capture(self):
         print '[Cancun] Camera Capture'
         image = self.mycamera.get_image()
-        pygame.image.save(image, "cancuncamera.jpg")
+        pygame.image.save(image, self.picturepygame)
+        commands.getstatusoutput(self.fswebcamcommand)
 
     def post(self):
         print '[Cancun] Camera Post'
