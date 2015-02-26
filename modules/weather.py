d020662de1263ad9c2b87d8da4cb3c7fcb584a08
Abraham Arce <xe1gyq@gmail.com>
2015-02-25 19:18:56 -0600

    Modules: Weather Yahoo API Handling
    Use specific API from pywapi to get Yahoo data

--- modules/weather.py
+++ modules/weather.py
@@ -8,6 +8,7 @@ import pywapi
 import string
 import json
 import urllib2
+import pprint
 
 from core.aprsfi import AprsFi
 from core.aprsnet import AprsNet
@@ -29,6 +30,8 @@ class Weather(object):
         self.agent = self.conf.get("weather", "agent")
 
         self.speaker = voicesynthetizer
+
+	self.pp = pprint.PrettyPrinter(indent=4)
 
     def personal(self):
 
@@ -62,36 +65,14 @@ class Weather(object):
         print '[Cancun] Weather Yahoo'
 
         yahoocityid = self.conf.get("weather", "yahoocityid")
-        self.url = "http://weather.yahooapis.com/forecastrss?w=" + yahoocityid  + "&u=c"
+        result = pywapi.get_weather_from_yahoo(yahoocityid, 'metric')
 
-        data = feedparser.parse(self.url)
-
-	# {'city': u'Guadalajara', 'region': u'JA', 'country': u'Mexico'}
-	# {'direction': u'0', 'speed': u'0', 'chill': u'22'}
-	# {'pressure': u'1008.2', 'rising': u'1', 'visibility': u'12.87', 'humidity': u'73'}
-	# {'sunset': u'8:32 pm', 'sunrise': u'7:12 am'}
-        
-        self.location = data.feed.yweather_location
-        self.wind = data.feed.yweather_wind
-        self.atmosphere = data.feed.yweather_atmosphere
-        self.astronomy = data.feed.yweather_astronomy
-
-        sunrise=self.astronomy['sunrise']
-        sunset=self.astronomy['sunset']
-
-        summary=data.entries[0].summary
-        temp=summary.split("High: ")
-        temp=temp[1].split(" Low: ")
-        self.high=temp[0]
-        self.low=temp[1].split("<br />")[0]
-
-        self.speaker.speechit("Reporte del clima en la ciudad de " + self.location['city'] + ", " + self.location['country'])
-        self.speaker.speechit("Temperatura maxima, " + self.high + " grados centigrados")
-        self.speaker.speechit("Temperatura minima, " + self.low + " grados centigrados")
-        self.speaker.speechit("Presion Atmosferica, " + self.atmosphere['pressure'] + " milibares")
-        self.speaker.speechit("Visibilidad, " + self.atmosphere['visibility'] + " kilometros")
-        self.speaker.speechit("Humedad, " + self.atmosphere['humidity'] + " por ciento")
-        self.speaker.speechit("El Sol se oculta a las " + self.astronomy['sunset'].replace(":", " "))
+        self.speaker.speechit("Reporte del clima en la ciudad de " + result['location']['city'])
+        self.speaker.speechit("Temperatura, " + result['condition']['temp'] + " grados centigrados")
+        self.speaker.speechit("Presion Atmosferica, " + result['atmosphere']['pressure'] + " milibares")
+        self.speaker.speechit("Visibilidad, " + result['atmosphere']['visibility'] + " kilometros")
+        self.speaker.speechit("Humedad, " + result['atmosphere']['humidity'] + " por ciento")
+        self.speaker.speechit("El Sol se oculta a las " + result['astronomy']['sunset'])
 
     def noaa(self):
 
