fcc111463cf51baf6d72678f6064866060d88e43
Abraham Arce <xe1gyq@gmail.com>
2015-02-28 14:05:25 -0600

    Modules: Alive System Information
    Core System gathers data from top and uname commands, include in
    our Alive report
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- modules/alive.py
+++ modules/alive.py
@@ -4,21 +4,24 @@ import logging
 import ConfigParser
 
 from core.randomizer import randomize
+from core.system import System
 from core.twitterc import TwitterC
 
 class Alive(object):
 
     def __init__(self):
 
-	self.twitterc = TwitterC('twython')
+        self.system = System()
+        self.twitterc = TwitterC('twython')
         self.conf = ConfigParser.ConfigParser()
         self.path = "configuration/general.config"
         self.conf.read(self.path)
 
     def report(self):
-        message = randomize(6) + ' ' + self.conf.get("system", "hashtag") + ' '
-        message = message + self.conf.get("general", "twitter") + ' '
-        message = message + self.conf.get("general", "radioclub") + ' @ ' + self.conf.get("general", "frequency")
+        system = self.system.execute()
+        message = randomize(2) + ' ' + self.conf.get("system", "hashtag") + ' '
+        message = message + self.conf.get("general", "twitter") + ' Frequency ' + self.conf.get("general", "frequency")
+        message = message + ' .. ' + system
         logging.info(message)
         self.twitterc.timeline_set(message, media=None)
         self.twitterc.timeline_get('arjaccancun', 1)
