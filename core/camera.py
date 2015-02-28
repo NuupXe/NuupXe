50d9dd4569e943a82304b17f0f6d38aa9f8cfd7a
Abraham Arce <xe1gyq@gmail.com>
2015-02-28 12:53:23 -0600

    Core: Camera Clean Up
    Some clean up required
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- core/camera.py
+++ core/camera.py
@@ -13,33 +13,31 @@ class Camera(object):
         self.voicesynthetizer = voicesynthetizer
         self.picturepygame = 'output/camerapygame.jpg'
         self.picturefswebcam = 'output/camerafswebcam.jpg'
-        self.fswebcam = 'fswebcam'
-        self.fswebcamarguments = ' -r 1280x1024 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner '
 
     def __del__(self):
         pass
 
     def setup(self):
         print '[Cancun] Camera Setup'
+
         pygame.camera.init()
         self.mycamera = pygame.camera.Camera("/dev/video0",(640,480))
         self.mycamera.start()
 
+        self.fswebcam = 'fswebcam'
+        self.fswebcamarguments = ' -r 1280x1024 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner '
         self.fswebcamcommand = self.fswebcam + self.fswebcamarguments + self.picturefswebcam
 
     def capture(self):
         print '[Cancun] Camera Capture'
+
         image = self.mycamera.get_image()
         pygame.image.save(image, self.picturepygame)
         self.mycamera.stop()
         status, output = commands.getstatusoutput(self.fswebcamcommand)
 
-    def post(self):
-        print '[Cancun] Camera Post'
-
     def execute(self):
         self.setup()
         self.capture()
-        self.post()
 
 # End of file
