605eaf7e87e6918217d32bacf73e9ff426893dd1
Abraham Arce <xe1gyq@gmail.com>
2015-03-15 20:03:17 -0600

    Core: Camera Resolution
    Set 1280x720 the capture resolution
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- core/camera.py
+++ core/camera.py
@@ -20,11 +20,11 @@ class Camera(object):
     def setup(self):
         logging.info('Camera Setup')
         pygame.camera.init()
-        self.mycamera = pygame.camera.Camera("/dev/video0",(640,480))
+        self.mycamera = pygame.camera.Camera("/dev/video0",(1280,720))
         self.mycamera.start()
 
         self.fswebcam = 'fswebcam'
-        self.fswebcamarguments = ' -r 1280x1024 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner '
+        self.fswebcamarguments = ' -r 1280x720 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner '
         self.fswebcamcommand = self.fswebcam + self.fswebcamarguments + self.picturefswebcam
 
     def capture(self):
