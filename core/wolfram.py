59692fa6b0a5d49e75052a2f941ac1a5618a9275
Abraham Arce <xe1gyq@gmail.com>
2015-03-15 22:39:38 -0600

    Core: Wolfram Question
    We have hardcoded 2 question, let's make this our core module by
    allowing others to ask one question at a time
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- core/wolfram.py
+++ core/wolfram.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 
 import ConfigParser
+import logging
 import re
 import wolframalpha
-
-from core.voicesynthetizer import VoiceSynthetizer
 
 class Wolfram(object):
 
@@ -12,29 +11,25 @@ class Wolfram(object):
 
         self.voicesynthetizer = voicesynthetizer
         
+    def setup(self):
+        logging.info('Wolfram Setup')
         self.conf = ConfigParser.ConfigParser()
         self.path = "configuration/services.config"
         self.conf.read(self.path)
-        self.questions = [
-        'what is the population in Mexico',
-        'how far is mexico from the US']
-
-    def identify(self):
-        print '[Cancun] Wolfram'
         appid=self.conf.get("wolfram", "appid")
         self.client = wolframalpha.Client(appid)
 
     def question(self, question):
-        self.identify()
+        logging.info('Wolfram Question')
+        self.setup()
         self.voicesynthetizer.setlanguage("english")
-        self.voicesynthetizer.speechit("Wolfram Alpha Experimental Module, Computational Knowledge Engine")
-        for question in self.questions:
-                res = self.client.query(question)
-                self.voicesynthetizer.speechit(question)
-                print(next(res.results).text)
-                string = re.sub('[^0-9a-zA-Z]+', ' ', next(res.results).text)
-                print string
-                self.voicesynthetizer.speechit(string)
+        logging.info("Wolfram Alpha Experimental Module, Computational Knowledge Engine")
+        res = self.client.query(question)
+        self.voicesynthetizer.speechit(question)
+        logging.info((next(res.results).text))
+        string = re.sub('[^0-9a-zA-Z]+', ' ', next(res.results).text)
+        logging.info(string)
+        self.voicesynthetizer.speechit(string)
         self.voicesynthetizer.setlanguage("spanish")
 
 # End of File
