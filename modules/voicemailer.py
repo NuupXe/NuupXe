b99e423cf9e24d7b706f93cf127eb174bd31e8d4
Abraham Arce <xe1gyq@gmail.com>
2015-03-29 18:03:32 -0600

    Modules: Voice Mailer Base Code
    You can send via email any voice message in both text and
    as audio file
    
    The list of users is given by the file
     configuration/voicemailer.config
    with this format
    
    [00]
    callsign=xe1xxx
    email=username@something.com
    
    Where 00 corresponds to the assigned number ID of the user
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ modules/voicemailer.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import ConfigParser
+import commands
+import logging
+
+from core.emailx import Emailx
+from core.irlp import Irlp
+from core.phonetic import Phonetic
+from core.pushtotalk import PushToTalk
+from core.voice import Voice
+from core.voicerecognition import VoiceRecognition
+
+class VoiceMailer(object):
+
+    def __init__(self, voicesynthetizer):
+
+        self.emailx = Emailx()
+        self.irlp = Irlp()
+        self.phonetic = Phonetic()
+        self.pushtotalk = PushToTalk()
+        self.voice = Voice()
+        self.voicerecognition = VoiceRecognition(voicesynthetizer)
+
+        self.conf = ConfigParser.ConfigParser()
+        self.filepath = "configuration/voicemailer.config"
+        self.conf.read(self.filepath)
+
+        self.voicesynthetizer = voicesynthetizer
+
+    def __del__(self):
+        pass
+
+    def decodeUser(self, dmtf):
+        logging.info('Voice Mailer Decode User')
+        user = dmtf[2:]
+        callsign = self.conf.get(user, "callsign")
+        email = self.conf.get(user, "email")
+        return callsign, email
+
+    def attend(self, dtmf):
+        logging.info('Voice Mailer Attend')
+
+        user, email = self.decodeUser(dtmf)
+        user = ' '.join(self.phonetic.decode(user))
+        self.voicesynthetizer.speechit("Mensaje para " + user)
+
+        self.voicesynthetizer.speechit('Cual es tu mensaje?')
+        self.voicerecognition.record()
+        message = self.voicerecognition.recognize('False')
+        logging.info('Mensaje? ' + message)
+        self.voicesynthetizer.speechit(message)
+        filename = self.voicerecognition.filegetname()
+        self.emailx.create(email, 'Proyecto Cancun Voice Mailer! Mensaje ...', message, filename)
+        self.emailx.send()
+
+# Enf of File
+
