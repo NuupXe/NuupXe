138ca3686e785ed45eafd8cf3ff7624e68039578
Abraham Arce <xe1gyq@gmail.com>
2015-04-01 21:22:37 -0500

    Modules: Alive Rework
    We might start seeing reworks like this specific on Alive
    module, anyway, we will be putting strong focus on making
    our code extremely easy to read
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- modules/alive.py
+++ modules/alive.py
@@ -21,32 +21,39 @@ class Alive(object):
     def setup(self):
 
         logging.info('Alive Setup')
+
         self.conf = ConfigParser.ConfigParser()
         self.path = "configuration/general.config"
         self.conf.read(self.path)
 
+        self.twitteraccount = self.conf.get("general", "twitter")
+        self.hashtag = self.conf.get("system", "hashtag")
+        self.systemfrequency =  self.conf.get("general", "frequency")
+        self.systemcoordinates = self.conf.get("general", "coordinates")
+
+        self.cpu = self.system.cpu()
+        self.memory = self.system.memory()
+        self.kernel = self.system.kernelVersion()
+
         callsign = self.conf.get("general", "callsign") + '-1'
         self.aprs.address_set(callsign)
-        self.aprs.position_set(self.conf.get("general", "coordinates"))
 
     def report(self):
 
         logging.info('Alive Report')
 
-        cpu = self.system.cpu()
-        cpu = 'Cpu ' + cpu + '%'
-        memory = self.system.memory()
-        memory = 'Memory ' + memory
-        kernel = self.system.kernelVersion()
-        kernel = 'Kernel ' + kernel
+        cpu = 'Cpu ' + self.cpu + '%'
+        memory = 'Memory ' + self.memory
+        kernel = 'Kernel ' + self.kernel
         system = ' ' + cpu + ' ' + memory + ' ' + kernel
 
-        message = Randomizer(2) + ' ' + self.conf.get("system", "hashtag") + ' '
-        message = message + self.conf.get("general", "twitter") 
-        technical = ' Freq ' + self.conf.get("general", "frequency") + system
+        message = Randomizer(2) + ' ' + self.hashtag + ' '
+        message = message + self.twitteraccount 
+        technical = ' Freq ' + self.systemfrequency + system
         message = message + ' ' + technical
 
         self.aprs.send_message(technical)
+        self.aprs.position_set(self.systemcoordinates)
         self.twitterc.timeline_set(message, media=None)
         logging.info(message)
 
