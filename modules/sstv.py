c39e5c6b83e46376d872912102eb9be34b7ed837
Abraham Arce <xe1gyq@gmail.com>
2015-04-26 10:59:27 -0500

    Modules: SSTV Typo + ModuleMessage +
    Fix one typo, pass modulemeesage to Alive module and change
    logging information when exception happens
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- modules/sstv.py
+++ modules/sstv.py
@@ -29,12 +29,12 @@ class SSTV(object):
             status, output = commands.getstatusoutput('python core/bing.py')
             status, output = commands.getstatusoutput('convert -resize 320x256\! output/bing.jpg output/radioclub.jpg')
             status, output = commands.getstatusoutput('python -m pysstv --mode MartinM1 --vox output/radioclub.jpg output/sstv.wav')
-            self.voicesynthetizer.speechit("Imagen lista! Comenzamos con la tranmision en Modo Martin Uno")
+            self.voicesynthetizer.speechit("Imagen lista! Comenzamos con la transmision en Modo Martin Uno")
             self.pushtotalk.openport()
             status, output = commands.getstatusoutput('aplay output/sstv.wav')
             self.pushtotalk.closeport()
-            alive(modulename=self.modulename, media='output/bing.jpg')
+            alive(modulename=self.modulename, modulemessage=None, media='output/bing.jpg')
         except:
-            logging.error('Cannot decode file')
+            logging.error('SSTV Decode Error')
 
 # End of File
