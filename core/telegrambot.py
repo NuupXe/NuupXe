65b7fdcc699be124bc07fc99ddab857cf8e6b56c
Abraham Arce <xe1gyq@gmail.com>
2015-12-22 13:27:11 -0500

    Core: Telegram Bot
    Telegram Bot Base Code
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ core/telegrambot.py
@@ -0,0 +1,31 @@
+import ConfigParser
+import logging
+import telebot
+
+class TelegramBot():
+
+    def __init__(self):
+        self.configuration = ConfigParser.ConfigParser()
+        self.configuration.read('configuration/services.config')
+        self.token = self.configuration.get('telegram','token')
+        self.mybot = telebot.TeleBot(self.token)
+
+    def listener(self, message):
+        for m in message:
+            chatid = m.chat.id
+            if m.content_type == 'text':
+                text = m.text
+                if m.text == '/date':
+                    self.mybot.send_message(chatid, text)
+                    self.mybot.send_message(chatid, "The date is ...")
+
+    def execute(self):
+        self.mybot.set_update_listener(self.listener)
+        self.mybot.polling()
+        while True:
+            pass
+
+if __name__ == "__main__":
+
+    telegrambot = TelegramBot()
+    telegrambot.execute()
