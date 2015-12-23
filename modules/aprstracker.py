5d868c80fa8d36744ed534072375c2f7e8a09c24
Abraham Arce <xe1gyq@gmail.com>
2015-12-23 11:10:15 -0500

    Modules: AprsTracker CleanUp
    Cleanup tasks
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- modules/aprstracker.py
+++ modules/aprstracker.py
@@ -30,9 +30,12 @@ months = {'January': 'Enero', 'February': 'Febrero', 'March': 'Marzo',
 
 class AprsTracker(object):
 
-    def __init__(self, voicesynthetizer):
-
+    def __init__(self, voicesynthetizer, callsign='XE1GYP-9'):
+        logging.info('[AprsTracker]')
+        self.speaker = voicesynthetizer
+        self.callsign = callsign
         self.modulename = 'AprsTracker'
+
         self.phonetic = Phonetic()
         self.aprsfi = AprsFi()
 
@@ -40,62 +43,58 @@ class AprsTracker(object):
         self.path = "configuration/general.config"
         self.conf.read(self.path)
 
-        self.speaker = voicesynthetizer
-        self.callsign = 'XE1GQP-9'
-
-    def clock(self, aprstime):
-
-        logging.info('Aprs Tracker Clock')
+    def time(self, aprstime):
+        logging.info('[AprsTracker] Time')
         weekday = days[time.strftime("%A", time.gmtime(int(aprstime)))]
         day = time.strftime("%d", time.gmtime(int(aprstime))).lstrip('0')
         month = months[time.strftime("%B", time.gmtime(int(aprstime)))]
         year = time.strftime("%Y", time.gmtime(int(aprstime)))
         return weekday, day, month, year
 
-    def query(self):
-
-        logging.info('Aprs Tracker Query')
+    def localize(self):
+        logging.info('[AprsTracker] Localize')
         self.speaker.speechit("Localizacion de estaciones a traves de a p r s punto f i")
-        self.localize(self.callsign)
-
-    def localize(self, callsign):
-
-        self.aprsfi.callsignset(callsign)
+        self.aprsfi.callsignset(self.callsign)
         self.aprsfi.dataset('loc')
         data = self.aprsfi.query()
         logging.info(data)
 
-        for entry in data['entries']:
-            station = "Estacion " + callsign
-            stationdecoded = "Estacion " + ' '.join(self.phonetic.decode(callsign))
-            weekday, day, month, year = self.clock(entry['lasttime'])
-            message = ", Ultima vez visto " + weekday + ' ' + day + ' de ' + month + ' del ' + year
-            try:
-                message =  message + ", Velocidad " + str(entry['speed']) + " Km/h"
-            except:
-                pass
-            try:
-                message =  message + ", Altitud " + str(entry['altitude']) + " metros"
-            except:
-                pass
-            results = Geocoder.reverse_geocode(float(entry['lat']), float(entry['lng']))
-            logging.info(results)
-            try:
-                message = message + ", Calle " + results[0].route
-                message = message + ", Colonia " + results[0].political
-                if results[0].administrative_area_level_2:
-                    message = message + ", Municipio " + results[0].administrative_area_level_2
-                elif results[0].locality:
-                    message =  message + ", Municipio " + results[0].locality
-                message =  message + ", " + results[0].administrative_area_level_1
-                message =  message + ", " + results[0].country
-            except:
-                pass
+        station = "Estacion " + self.callsign
+        stationdecoded = "Estacion " + ' '.join(self.phonetic.decode(self.callsign))
 
-            speechmessage = stationdecoded + message
-            self.speaker.speechit(speechmessage)
+        if data.get('entries'):
+            for entry in data['entries']:
+                weekday, day, month, year = self.time(entry['lasttime'])
+                message = ", Ultima vez visto " + weekday + ' ' + day + ' de ' + month + ' del ' + year
+                try:
+                    message =  message + ", Velocidad " + str(entry['speed']) + " Km/h"
+                except:
+                    pass
+                try:
+                    message =  message + ", Altitud " + str(entry['altitude']) + " metros"
+                except:
+                    pass
+                results = Geocoder.reverse_geocode(float(entry['lat']), float(entry['lng']))
+                logging.info(results)
+                try:
+                    message = message + ", Calle " + results[0].route
+                    message = message + ", Colonia " + results[0].political
+                    if results[0].administrative_area_level_2:
+                        message = message + ", Municipio " + results[0].administrative_area_level_2
+                    elif results[0].locality:
+                        message =  message + ", Municipio " + results[0].locality
+                    message =  message + ", " + results[0].administrative_area_level_1
+                    message =  message + ", " + results[0].country
+                except:
+                    pass
 
-        modulemessage = station + message
-        alive(modulename=self.modulename, modulemessage=modulemessage)
+                speechmessage = stationdecoded + message
+                self.speaker.speechit(speechmessage)
+
+            modulemessage = station + message
+            alive(modulename=self.modulename, modulemessage=modulemessage)
+
+        else:
+            self.speaker.speechit(stationdecoded + " no ha reportado ubicacion!")
 
 # End of File
